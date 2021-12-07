2d16f4ddf08c5cb3e27934777dd5f108a7255ebd
Priyansh Sharma <sharma.priyansh@affixious.com>
2021-12-07 14:54:53 +0530

    connection updated

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(`mongodb://${process.env.DB_DOMAIN}:${process.env.DB_PORT}/todoApp`, {
+mongoose.connect(process.env.DB_URI_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("Database connection succesful"))
-.catch(err => console.log("database not connected", err))
+    .then(() => console.log("Database connection succesful"))
+    .catch(err => console.log("database not connected", err))
